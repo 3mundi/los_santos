eb9bd674dc7993e87e2d72b94a388998b53be32d
sirjetboy <robin.anselmet@fr.fcm.travel>
2017-11-14 10:49:06 +0100

    last rubocop change

--- lib/los_santos.rb
+++ lib/los_santos.rb
@@ -21,11 +21,8 @@ module LosSantos
 
   def self.parse_string(date)
     regexp = /[0-9]{4}-[0-9]{2}-[0-9]{2}/
-    if regexp === date
-      return Date.parse(date)
-    else
-      raise ArgumentError, "String usage: LosSantos.name_day 'yyyy-mm-dd'"
-    end
+    return Date.parse(date) if date =~ regexp
+    raise ArgumentError, "String usage: LosSantos.name_day 'yyyy-mm-dd'"
   end
 
   def self.load_data
