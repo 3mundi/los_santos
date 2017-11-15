23ff675e8a9fd64a126f7291760b584fb7522b96
sirjetboy <robin.anselmet@fr.fcm.travel>
2017-11-15 14:10:24 +0100

    name_day? no case sensitive

--- lib/los_santos/extensions/string.rb
+++ lib/los_santos/extensions/string.rb
@@ -1,6 +1,12 @@
 # implement method name_day? to the class String
+require 'rubygems'
+require 'active_support/inflector'
+
 class String
   def name_day?(date = Time.now.localtime)
-    LosSantos.name_day(date).include? self
+    LosSantos.name_day(date).each do |name|
+      return true if name.parameterize == self.parameterize
+    end
+    false
   end
 end
--- spec/los_santos/extensions/string_spec.rb
+++ spec/los_santos/extensions/string_spec.rb
@@ -7,5 +7,10 @@ RSpec.describe String do
     it 'should return true' do
       expect('Emile'.name_day?(Time.new(1993, 5, 22))).to be true
     end
+
+    it 'should not be case sensitive' do
+      expect('émile'.name_day?(Time.new(1993, 5, 22))).to be true
+      expect('JEAN BAPTISTE'.name_day?(Time.new(1993, 4, 7))).to be true
+    end
   end
 end
